da4a69f3d5b3f4242daf9c0c1a177cd1e43e871b
omar <omar@DESKTOP-JO343MQ>
2019-05-27 20:33:44 -0500

    Ejercicio 7

---
+++ Ejercicio7/src/E7.java
@@ -0,0 +1,26 @@
+
+public class E7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+int i, x=4, w=9, q;
+		
+		for(i=-1; i<20; i=i-3) {
+			x++;
+			for(q=4; q<11; q++) {
+				do {
+					i=i+3;
+					w=(4);
+					i=x+w;
+					x=w+i;
+				} while(x<15);
+			}
+		}
+		System.out.println("x:"+x+" i:"+i);
+
+	
+
+	}
+
+}
